9775a0fa33c06a9d9ce5b371cfa20e5e2ae8aa89
Ronny Wang <ronnywang@gmail.com>
2014-01-03 17:11:06 +0800

    加上 NotifyLib::alert(title, body) 寄信通知

---
+++ webdata/models/NotifyLib.php
@@ -0,0 +1,34 @@
+<?php
+
+class NotifyLib
+{
+    public function alert($title, $body)
+    {
+        if (!class_exists('AmazonSES')) {
+            define('AWS_DISABLE_CONFIG_AUTO_DISCOVERY', true);
+            include(__DIR__ . '/../stdlibs/sdk-1.6.0/sdk.class.php');
+            if (!getenv('SES_KEY') or !getenv('SES_SECRET')) {
+                throw new Exception('env SES_KEY & SES_SECRET not found');
+            }
+            CFCredentials::set(array(
+                'development' => array(
+                    'key' => getenv('SES_KEY'),
+                    'secret' => getenv('SES_SECRET'),
+                ),
+            ));
+        }
+        $ses = new AmazonSES();
+        //$ses->set_region(AmazonSES::REGION_TOKYO);
+        $ret = $ses->send_email(
+            getenv('SES_MAIL'),
+            array(
+                'ToAddresses' => array(getenv('SES_MAIL')),
+            ),
+            array(
+                'Subject.Data' => $title,
+                'Body.Text.Data' => $body,
+            )
+        );
+        var_dump($ret);
+    }
+}
