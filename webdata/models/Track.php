5eb174acb1203a56936b9b7abcefb0da38a9155d
Ronny Wang <ronnywang@gmail.com>
2014-01-03 16:28:11 +0800

    加上 Track->title 欄位

--- webdata/models/Track.php
+++ webdata/models/Track.php
@@ -87,6 +87,7 @@ class Track extends Pix_Table
         $this->_rowClass = 'TrackRow';
 
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
+        $this->_columns['title'] = array('type' => 'text');
         $this->_columns['created_at'] = array('type' => 'int');
         $this->_columns['tracked_at'] = array('type' => 'int');
         $this->_columns['url'] = array('type' => 'varchar', 'size' => 255);
