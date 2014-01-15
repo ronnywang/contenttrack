1184c36a56f22611375ad792d360d9b7bcf7825c
Ronny Wang <ronnywang@gmail.com>
2014-01-16 00:36:09 +0800

    加上登入功能

---
+++ webdata/models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+class User extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'user';
+
+        $this->_primary = 'user_id';
+
+        $this->_columns['user_id'] = array('type' => 'int', 'auto_increment' => true);
+        $this->_columns['user_name'] = array('type' => 'varchar', 'size' => 64);
+
+        $this->addIndex('user_name', array('user_name'), 'unique');
+
+        $this->_relations['user_teams'] = array('rel' => 'has_many', 'type' => 'TeamMember', 'foreign_key' => 'user_id');
+    }
+}
