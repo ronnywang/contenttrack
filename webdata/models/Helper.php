f3392795ba5a129519c1bc64fefee8c152345352
Ronny Wang <ronnywang@gmail.com>
2016-05-06 01:03:33 +0800

    decode_utf8 加上 static

--- webdata/models/Helper.php
+++ webdata/models/Helper.php
@@ -2,7 +2,7 @@
 
 class Helper
 {
-    public function decode_utf8($str)
+    public static function decode_utf8($str)
     {
         return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
                 return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
